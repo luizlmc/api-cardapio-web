c1adf24ba7fed8fae828b12bc1f6b37cf257db08
luizlmc <luiz.carvalho.lmc@gmail.com>
2023-04-18 19:45:17 -0300

    First commit

---
+++ src/main/java/br/com/luizlmc/apicardapioweb/ApiCardapioWebApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizlmc.apicardapioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCardapioWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCardapioWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/luizlmc/apicardapioweb/ApiCardapioWebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luizlmc.apicardapioweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCardapioWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
